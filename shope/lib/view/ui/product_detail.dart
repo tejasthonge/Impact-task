6e980b399c70f3b7089ceb0d3f6cec2b228d3183
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-10-26 10:19:17 +0530

    all Done

--- shope/lib/view/ui/product_detail.dart
+++ shope/lib/view/ui/product_detail.dart
@@ -24,7 +24,7 @@ class _ProductDeatilPageState extends State<ProductDeatilPage> {
   void initState() {
     super.initState();
     productDetail = productController.getProductDetailById(widget.productID);
-    log(productDetail.discount);
+    log(productDetail.productDetails.details1);
   }
 
   @override
@@ -45,33 +45,38 @@ class _ProductDeatilPageState extends State<ProductDeatilPage> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     getBasicInfo(),
                     getPriceInfo(),
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [ 
-
-                        Text("Available Options",
-                        style: GoogleFonts.inter( 
-                          fontWeight: FontWeight.bold
-                        ),),
-
-                      ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: productDetail.availableSizes.length,
-                        itemBuilder: (context,index){
-                          var size = productDetail.availableSizes[index];
-                        return Container( 
-                          child: Text(size),
-                        )
-                      })
-                      ],
-                    )
+                    getAvailableOptions(),
+                    getSizeBox(),
                   ],
                 ),
-              )
+              ),
+             const Divider(
+                color: const Color.fromARGB(105, 239, 222, 222),
+                thickness: 9,
+              ),
+
+              Padding(
+                   padding:  
+                   const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+
+                child: Column( 
+                  children: [ 
+                    getDelivrySection(),
+                    
+                  ],
+                ),
+              ),
+              const  Divider(
+                color: const Color.fromARGB(105, 239, 222, 222),
+                thickness: 9,
+              ),
+
+              getProductDetail()
+
             ],
           ),
         ),
@@ -140,6 +145,181 @@ class _ProductDeatilPageState extends State<ProductDeatilPage> {
           ),
         ),
       ),
+    );
+  }
+
+  Padding getProductDetail() {
+    return Padding(padding:  
+                 const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            
+            child:Column( 
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [ 
+                      Text("Product Details",style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600,decoration: TextDecoration.underline)),
+
+                      Row( 
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [ 
+                          Expanded(
+                            flex: 5,
+                            child: Column( 
+                                mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [ 
+                              Text(productDetail.productDetails.details1,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+                              Text(productDetail.productDetails.details2,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+
+                              Text(productDetail.productDetails.details3,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+
+                              Text(productDetail.productDetails.details4,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+
+                              Text(productDetail.productDetails.details5,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+
+                            ],
+                          )),
+                          Expanded(
+                            flex: 5,
+                            child: Column( 
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [ 
+                             
+                              Text(productDetail.productDetails.details3,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                              const SizedBox(height: 10,),
+
+                              Text(productDetail.productDetails.details4,
+                              style: GoogleFonts.inter
+                              (fontWeight: FontWeight.w600),),
+                             
+                            ],
+                          ))
+                        ],
+                      )
+
+                      
+                    ],
+                  ) ,);
+  }
+
+  Column getDelivrySection() {
+    return Column(
+                    crossAxisAlignment:  CrossAxisAlignment.start,
+                    children: [
+                      Text("Delivery and service",
+                      style: GoogleFonts.inter
+                      (fontWeight: FontWeight.w400),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 6,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Enter Pincode",
+                                border: OutlineInputBorder(),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.green)),
+                                errorBorder: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+
+                              alignment: Alignment.center,
+                              height: 50,
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              
+                              margin: EdgeInsets.only(left: 20),
+                              decoration: BoxDecoration( 
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.green.shade900
+                              ),
+                              child: Text(
+                                "Verify",
+                                style: GoogleFonts.inter(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  );
+  }
+
+  Column getAvailableOptions() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Available Options",
+          style: GoogleFonts.inter(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Column getSizeBox() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Selected Size :${productDetail.selectedSize}"),
+            Text(
+              "Size Chart",
+              style: GoogleFonts.inter(decoration: TextDecoration.underline),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: productDetail.availableSizes.length,
+              itemBuilder: (context, index) {
+                var size = productDetail.availableSizes[index];
+                log(productDetail.availableSizes.length.toString());
+                return Container(
+                  height: 50,
+                  width: 50,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.green.withOpacity(0.1),
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: productDetail.selectedSize == size
+                              ? Colors.green.shade700
+                              : Colors.green.shade300)),
+                  child: Text(size),
+                );
+              }),
+        ),
+      ],
     );
   }
 
---
+++ shope/lib/view/widgets/index.dart
@@ -0,0 +1,2 @@
+export 'app_bar_widget.dart';
+export 'product_list_widget.dart';
